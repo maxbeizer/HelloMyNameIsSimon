2d816d42b3aa169468a0aeb0376099e0ec934cb2
Max Beizer <max.beizer@gmail.com>
2015-02-11 20:01:46 -0600

    Draw my drawrings

--- HelloMyNameIsSimon/DrawView.swift
+++ HelloMyNameIsSimon/DrawView.swift
@@ -9,32 +9,52 @@
 import UIKit
 
 class DrawView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
     
-    // NSSet is guaranteed to have unique value
+    var allPoints = [CGPoint]()
+
+    override func drawRect(rect: CGRect) {
+        let currentContext = UIGraphicsGetCurrentContext()
+        self.drawPoints(self.allPoints, context: currentContext)
+    }
+    
+    func drawPoints(points: [CGPoint], context: CGContextRef) {
+        if points.count > 1 {
+            let path = CGPathCreateMutable()
+            let firstPoint = points[0]
+            CGPathMoveToPoint(path, nil, firstPoint.x, firstPoint.y)
+            
+            for pointIndex in 1..<points.count {
+                let point = points[pointIndex] as CGPoint
+                CGPathAddLineToPoint(path, nil, point.x, point.y)
+            }
+            
+            CGContextSetStrokeColorWithColor(context, UIColor.blackColor().CGColor)
+            CGContextSetLineWidth(context, 5.0)
+            CGContextAddPath(context, path)
+            CGContextDrawPath(context, kCGPathStroke)
+        }
+    }
+
+    // NSSet is a collection guaranteed to have unique values
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         let touch = touches.anyObject() as UITouch
         let point = touch.locationInView(self)
-        println(point)
+        self.allPoints.append(point)
+        self.setNeedsDisplay()
     }
     
     override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
         let touch = touches.anyObject() as UITouch
         let point = touch.locationInView(self)
-        println(point)
+        self.allPoints.append(point)
+        self.setNeedsDisplay()
     }
     
     override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
         let touch = touches.anyObject() as UITouch
         let point = touch.locationInView(self)
-        println(point)
+        self.allPoints.append(point)
+        self.setNeedsDisplay()
     }
 
 }
